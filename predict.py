10037f15256674957a8db1e8f8257b9fab13677a
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2018-04-04 20:10:48 -0300

    :hammer: Modelo de predicao

--- predict.py
+++ predict.py
@@ -1,6 +1,12 @@
 import os
 import logging
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.misc import imresize
+from keras.applications import inception_v3
 from keras.models import Model, load_model
+from keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import accuracy_score, confusion_matrix
 #################################################################
 #               Configurando logs de execucao                   #
@@ -13,15 +19,90 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
 
+#################################################################
+#               Configurando logs de execucao                   #
+#################################################################
+def carregar_triplas(lista_triplas):
+    image_triples = pd.read_csv(lista_triplas, sep=",", header=0, names=["left","right","similar"])
+    return image_triples.values
+#################################################################
+def load_image_cache(image_cache, image_filename):
+    image = plt.imread(os.path.join(IMAGE_DIR, image_filename))
+    image = imresize(image, (299, 299))
+    image = image.astype("float32")
+    image = inception_v3.preprocess_input(image)
+    image_cache[image_filename] = image
+################################################################
+def pair_generator(triples, image_cache, datagens, batch_size=32):
+    while True:
+        # shuffle once per batch
+        indices = np.random.permutation(np.arange(len(triples)))
+        num_batches = len(triples) // batch_size
+        for bid in range(num_batches):
+            batch_indices = indices[bid * batch_size : (bid + 1) * batch_size]
+            batch = [triples[i] for i in batch_indices]
+            X1 = np.zeros((batch_size, 299, 299, 3))
+            X2 = np.zeros((batch_size, 299, 299, 3))
+            Y = np.zeros((batch_size, 2))
+            for i, (image_filename_l, image_filename_r, label) in enumerate(batch):
+                if datagens is None or len(datagens) == 0:
+                    X1[i] = image_cache[image_filename_l]
+                    X2[i] = image_cache[image_filename_r]
+                else:
+                    X1[i] = datagens[0].random_transform(image_cache[image_filename_l])
+                    X2[i] = datagens[1].random_transform(image_cache[image_filename_r])
+                Y[i] = [1, 0] if label == 0 else [0, 1]
+            yield [X1, X2], Y
+################################################################
+
 DATA_DIR = os.environ["DATA_DIR"]
 FINAL_MODEL_FILE = os.path.join(DATA_DIR, "models", "inception-ft-final-500.h5")
+TRIPLES_FILE = os.path.join(DATA_DIR, "triplas_imagenet_vqa.csv") 
+IMAGE_DIR = os.path.join(DATA_DIR,"imagenet_vqa")
+
 
 logger.debug("DATA_DIR %s", DATA_DIR)
 logger.debug("FINAL_MODEL_FILE %s", FINAL_MODEL_FILE)
+logger.debug("TRIPLES_FILE %s", TRIPLES_FILE)
+logger.debug("IMAGE_DIR %s", IMAGE_DIR)
+
+triples_data = carregar_triplas(TRIPLES_FILE)
 
 
+logger.debug( "Numero de pares : %d",  len(triples_data))
+
+image_cache = {}
+num_pairs = len(triples_data)
+logger.debug( "carregando imagens")
+
+for i, (image_filename_l, image_filename_r, _) in enumerate(triples_data):    
+    if image_filename_l not in image_cache:
+        load_image_cache(image_cache, image_filename_l)
+    if image_filename_r not in image_cache:
+        load_image_cache(image_cache, image_filename_r)
+
+logger.info("imagens carregadas")
+
+
+logger.info("gerando dados a partir das imagens")
+datagen_args = dict(rotation_range=10,
+                    width_shift_range=0.2,
+                    height_shift_range=0.2,
+                    zoom_range=0.2)
+datagens = [ImageDataGenerator(**datagen_args),
+            ImageDataGenerator(**datagen_args)]
+pair_gen = pair_generator(triples_data, image_cache, datagens, 32)
+[X1, X2], Y = pair_gen.__next__()
+
+logger.info("pronto")
+
+"""
+logger.info("Carregando o modelo")
 final_model = load_model(FINAL_MODEL_FILE)
+logger.info("Modelo carregado com sucesso")
 
-Ytest_ = model.predict()
+X1test, X2test = [], []
+Ytest_ = final_model.predict([X1test, X2test])
+"""
 
 logger.info("Finalizado")
