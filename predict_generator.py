174a843cb83e6e94a468f82762a6060e520d3129
Ramon Silva <ramon.silva@m4u.com.br>
2018-12-26 18:16:23 -0200

    [abc-0011] - gerando triplas

--- predict_generator.py
+++ predict_generator.py
@@ -27,12 +27,10 @@ logging.basicConfig(level=logging.DEBUG,
                     filemode='w')
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
-
 #################################################################
 def pair_generator(triples, image_cache, datagens, batch_size=32):
-    while True:
-        # shuffle once per batch
-        indices = np.random.permutation(np.arange(len(triples)))
+    while True:        
+        indices = np.array( range(0, len(triples)-1))
         num_batches = len(triples) // batch_size
         for bid in range(num_batches):
             batch_indices = indices[bid * batch_size : (bid + 1) * batch_size]
@@ -57,6 +55,21 @@ def carregar_triplas(lista_triplas):
     image_triples = pd.read_csv(lista_triplas, sep=",", header=0, names=["left","right","similar"])
     return image_triples.values
 #################################################################
+def obter_nome_arquivos_imagenet(synset_list):
+    imagenet_data = []
+    for synset in synsets:        
+        logger.info("carregando o synset [%s]", synset[0])
+        synset_path = os.path.join(IMAGENET_DIR, synset[0])
+        
+        if os.path.isdir(synset_path):
+            for imagenet_file in os.listdir(synset_path):
+                imagenet_data.append(os.path.join(synset[0], imagenet_file))
+                load_image_cache(image_cache, os.path.join(synset[0], imagenet_file), IMAGENET_DIR)  
+        else:
+            logger.warn("%s nao existe", synset_path) 
+    return imagenet_data        
+
+#################################################################
 def load_image_cache(image_cache, image_filename, directory):
     image = plt.imread(os.path.join(directory, image_filename))
     image = imresize(image, (299, 299))
@@ -64,6 +77,15 @@ def load_image_cache(image_cache, image_filename, directory):
     image = inception_v3.preprocess_input(image)
     image_cache[image_filename] = image
 ################################################################
+def gerar_triplas(vqa_list, imagenet_list):
+    triples = []
+    for vqa_file in vqa_filenames_list:
+        vqa_img = vqa_file[0]
+        for imagenet_img in imagenet_list:
+            triples.append([vqa_img, imagenet_img])
+    return triples
+
+#################################################################
 DATA_DIR = os.environ["DATA_DIR"]
 #FINAL_MODEL_FILE = os.path.join(DATA_DIR, "models", "inception-ft-best.h5")
 FINAL_MODEL_FILE = os.path.join(DATA_DIR, "vqa", "models", "distilation","inception-training-distlation3-ft-best.h5")
@@ -90,8 +112,16 @@ model = load_model(FINAL_MODEL_FILE)
 logger.info("Modelo carregado com sucesso")
 ################################################################
 logger.debug( "Carregando pares de imagens...")
+
+synsets = load_synset_list(os.path.join(DATA_DIR, "LOC_synset_mapping.txt"))
+synsets = synsets[0:20]
+imagenet_filenames_list = obter_nome_arquivos_imagenet(synsets)
+
+vqa_filenames_list = load_vqa_filenames_list(os.path.join(DATA_DIR, "mscoco_cats.csv"))
+
 image_cache = {}
-triples_data = carregar_triplas(TRIPLES_FILE)
+triples_data = gerar_triplas(vqa_filenames_list, imagenet_filenames_list)
+
 num_pairs = len(triples_data)
 logger.info("num pares %s", num_pairs)
 ################################################################
